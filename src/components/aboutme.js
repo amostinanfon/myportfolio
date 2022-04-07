573e7848c515ab3bb3af6e27352adfbe8829f597
amostinanfon <amostinanfon17@gmail.com>
2022-04-07 17:24:57 +0100

    styling About me component

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import image from '../../src/assets/images/image00.png'
+//import photo from '../../src/assets/images/photo.PNG'
+
+
 
 export default class Aboutme extends Component {
     render() {
         return (
             <div>
                 <h1>Qui suis-je ???</h1>
+                <img src={image} alt='photo2'/>
+                <p>Dolor amet nostrud ullamco sunt aliqua aliquip. Mollit id do ut consectetur occaecat ad est Lorem in incididunt enim elit esse elit. Voluptate velit laborum et reprehenderit enim ea ipsum aliquip esse proident amet non. Exercitation ad Lorem ea consectetur aliquip sint culpa. Elit quis consequat enim proident laboris duis exercitation nulla irure laboris voluptate. Aliquip nostrud dolor nulla anim Lorem.
+Laboris veniam dolore ipsum consequat nulla nisi. Irure nostrud labore veniam qui labore cupidatat. Ipsum ex proident cupidatat aliqua magna est sunt dolore exercitation deserunt. Ipsum id incididunt do minim laborum. Aliquip in do veniam nulla officia ad commodo magna nisi do fugiat eiusmod fugiat. Labore est in magna officia exercitation exercitation. Pariatur enim cillum qui ea labore duis et ut Lorem sit excepteur qui et do.
+Cupidatat irure aliqua velit pariatur eiusmod culpa cupidatat exercitation exercitation cupidatat. Excepteur labore eu quis irure exercitation. Ad cillum Lorem cupidatat cillum est excepteur ut tempor anim laboris nulla elit veniam Lorem. Consequat exercitation cupidatat proident fugiat nostrud incididunt anim ut irure laborum exercitation. Reprehenderit sit consectetur laboris occaecat voluptate cupidatat dolor.
+Nostrud reprehenderit cupidatat ipsum cillum anim laboris ex aliquip esse consectetur. Ad laboris dolor sint eiusmod nisi nostrud exercitation adipisicing deserunt consequat ad officia. Mollit proident ea eiusmod est duis ipsum. Nisi deserunt nulla amet magna elit consectetur dolore id eu dolore. Mollit est ad cillum ipsum mollit. Minim culpa duis irure magna labore. Ex excepteur pariatur officia eiusmod incididunt anim nulla qui commodo eiusmod officia aliquip et do.</p>
             </div>
         )
     }
