785c05c44c275e948876e6584eb596fb340d291e
amostinanfon <amostinanfon17@gmail.com>
2022-08-06 20:10:39 +0100

    Updates

---
+++ precache-manifest.5800726740fc653702cc97de51b0b994.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd66044a2a347443b75f614c69256c4d",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "ec20530f2ea16c7541e4",
+    "url": "/myportfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "0c57e72fa5975eea3fdb",
+    "url": "/myportfolio/static/css/main.2ed0f0a8.chunk.css"
+  },
+  {
+    "revision": "ec20530f2ea16c7541e4",
+    "url": "/myportfolio/static/js/2.7c9f2aff.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/myportfolio/static/js/2.7c9f2aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c57e72fa5975eea3fdb",
+    "url": "/myportfolio/static/js/main.f7c6f20d.chunk.js"
+  },
+  {
+    "revision": "da2e02db1e870a8bb8e5",
+    "url": "/myportfolio/static/js/runtime-main.b5f0a273.js"
+  },
+  {
+    "revision": "c91e5376a37e398b50153b4ebad0bb12",
+    "url": "/myportfolio/static/media/aboutme.c91e5376.jpg"
+  }
+]);
